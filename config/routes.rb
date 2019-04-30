1b8733c97bd2dccc1ad3c978d4524cf1c3da8b4b
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-04-30 15:29:16 +0200

    scaffolds new rails app, practices nesting and namespacing, creates custom items

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  namespace :admin do
+  get 'dashboard/main'
+  get 'dashboard/user'
+  get 'dashboard/blog'
+  end
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  resources :blogs
+
+  root to: 'pages#home'
+end
